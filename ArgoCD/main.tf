54c4b2484fbeef72e826bb513a7332643b87824b
7vishalroy <175200845+vishalgitt7@users.noreply.github.com>
2025-06-29 19:14:39 +0530

    Update main.tf

--- ArgoCD/main.tf
+++ ArgoCD/main.tf
@@ -1,14 +1,14 @@
 # Configure the Kubernetes provider
    provider "kubernetes" {
      config_path    = "~/.kube/config"
-     config_context = "arn:aws:eks:us-west-2:<account-id>:cluster/<cluster-name>" # Replace with your cluster context
+     config_context = "arn:aws:eks:us-east-1:546151329042:cluster/my-cluster" # Replace with your cluster context
    }
 
    # Configure the Helm provider
    provider "helm" {
-     kubernetes {
+     kubernetes = {
        config_path    = "~/.kube/config"
-       config_context = "arn:aws:eks:us-west-2:<account-id>:cluster/<cluster-name>"
+       config_context = "arn:aws:eks:us-east-1:546151329042:cluster/my-cluster"
      }
    }
 
@@ -28,37 +28,12 @@
      version    = "7.6.7" # Specify a stable chart version
 
      # Configure NodePort service
-     set {
+     set = [
+     {
        name  = "server.service.type"
-       value = "NodePort"
+       value = "LoadBalancer"
      }
-     set {
-       name  = "server.service.nodePort"
-       value = "30080" # Optional: Specify a port; omit for auto-assignment
-     }
-     set {
-       name  = "server.insecure"
-       value = "true" # For testing; disable in production
-     }
+    ]
 
      depends_on = [kubernetes_namespace.argocd]
-   }
-
-   # Output the NodePort
-   data "kubernetes_service" "argocd_server" {
-     metadata {
-       name      = "argocd-server"
-       namespace = kubernetes_namespace.argocd.metadata[0].name
-     }
-     depends_on = [helm_release.argocd]
-   }
-
-   output "argocd_node_port" {
-     value       = data.kubernetes_service.argocd_server.spec[0].port[0].node_port
-     description = "The NodePort assigned to the ArgoCD server"
-   }
-
-   output "argocd_cluster_ip" {
-     value       = data.kubernetes_service.argocd_server.spec[0].cluster_ip
-     description = "The ClusterIP of the ArgoCD server"
    }
